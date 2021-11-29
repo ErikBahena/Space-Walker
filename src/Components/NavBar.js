1d69b40824ad3be2acde55391cbbe7ecef012bee
[Erik Bahena] <[erikjbahena@gmail.com]>
2021-11-29 07:16:19 -0800

    making nav responsive

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -17,6 +17,60 @@ const StyledNav = styled.nav`
   height: max-content;
   width: 100%;
 
+  #menuIcon {
+    display: flex;
+    flex-direction: column;
+    transform: scale(0.7);
+
+    width: 70px;
+    margin-right: 4%;
+    cursor: pointer;
+
+    @media (min-width: 701px) {
+      display: none;
+    }
+
+    span {
+      background: #fff;
+      border-radius: 10px;
+      height: 4px;
+      margin: 7px 0;
+      transition: 0.4s cubic-bezier(0.68, -0.6, 0.32, 1.6);
+    }
+
+    span:nth-of-type(1) {
+      width: 50%;
+    }
+
+    span:nth-of-type(2) {
+      width: 100%;
+    }
+
+    span:nth-of-type(3) {
+      width: 75%;
+    }
+
+    input[type="checkbox"] {
+      display: none;
+    }
+
+    input[type="checkbox"]:checked ~ span:nth-of-type(1) {
+      transform-origin: bottom;
+      transform: rotatez(45deg) translate(8px, 0px);
+    }
+
+    input[type="checkbox"]:checked ~ span:nth-of-type(2) {
+      transform-origin: top;
+      transform: rotatez(-45deg);
+    }
+
+    input[type="checkbox"]:checked ~ span:nth-of-type(3) {
+      transform-origin: bottom;
+      width: 50%;
+      transform: translate(30px, -11px) rotatez(45deg);
+    }
+  }
+
   .nav-links-container {
     display: flex;
     align-items: center;
@@ -30,6 +84,32 @@ const StyledNav = styled.nav`
 
     .navText {
       text-transform: uppercase;
+    }
+
+    .close-menu-btn {
+      display: none;
+      transform: scale(3);
+      margin-left: 10px;
+
+      .chevron::before {
+        border-style: solid;
+        border-color: white;
+        border-width: 0.15em 0.15em 0 0;
+        content: "";
+        display: inline-block;
+        height: 0.45em;
+        left: 0.15em;
+        position: relative;
+        top: 0.15em;
+        transform: rotate(-45deg);
+        vertical-align: top;
+        width: 0.45em;
+      }
+
+      .chevron.right:before {
+        left: 0;
+        transform: rotate(45deg);
+      }
     }
 
     a {
@@ -90,12 +170,44 @@ const StyledNav = styled.nav`
   // Mobile Responsiveness
   @media (max-width: 700px) {
     padding-top: 1.5rem;
+
     .nav-logo {
       height: 40px;
       width: 40px;
     }
+
     .nav-links-container {
       display: none;
+    }
+
+    .nav-links-container.responsive-active {
+      .close-menu-btn {
+        display: unset;
+        align-self: flex-start;
+      }
+
+      position: absolute;
+      z-index: 999;
+      display: flex;
+      flex-direction: column;
+      top: 0;
+
+      right: 0;
+
+      height: 100%;
+
+      gap: 10%;
+
+      a {
+        height: unset;
+        width: 70%;
+
+        display: flex;
+        justify-content: flex-start;
+
+        padding: 0 1em;
+        margin-right: 0px !important;
+      }
     }
   }
 `;
@@ -120,13 +232,32 @@ export default function NavBar({ currentPage, setCurrentPage }) {
     });
   };
 
+  const toggleNav = () => {
+    const nav = document.querySelector(".nav-links-container");
+    nav.classList.toggle("responsive-active");
+    console.log("being called");
+
+    console.log(nav);
+  };
+
   return (
     <StyledNav>
       <Link to="/">
         <img className="nav-logo" src={NavLogo} alt="home page link" />
       </Link>
 
+      <label htmlFor="check" id="menuIcon">
+        <input type="checkbox" id="check" onClick={toggleNav} />
+        <span></span>
+        <span></span>
+        <span></span>
+      </label>
+
       <div onClick={changePage} className="nav-links-container">
+        <div className="close-menu-btn">
+          <span class="chevron right"></span>
+        </div>
+
         <Link
           to="/"
           data-name="home"
